d2f53cfdae6baf279efc67627fa75dbf575d0953
MrMark1127 <mark11273@gmail.com>
2022-12-04 22:06:50 -0500

    Initial Commit

---
+++ Day-1/puzzle-1.js
@@ -0,0 +1,48 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+const { start } = require('repl');
+let input
+
+function syncReadFile(filename) {
+    const contents = readFileSync(filename, 'utf-8')
+    input = contents.split(/\r?\n/)
+    return input
+}
+syncReadFile("./input.txt")
+
+function DoPuzzle(input) {
+    //Sets the array to integers, not string. Undefined if they're not a number. Thanks @ItsANoBrainer.
+    input = input.map(x => 
+        parseInt(x) || undefined
+    )
+
+    //Initialize total table and a temp total table, thanks again @ItsANoBrainer.
+    let total = []
+    let temptotal = []
+
+    //For each number at the input
+    input.forEach(num =>{
+        if (num != undefined) {             //If the number is not undefined, push it to another array
+            temptotal.push(num)
+        } else {
+            total.push(temptotal)           //When the number is unidentified, push the previous numbers to the total numbers array, reset temp array
+            temptotal = []
+        }
+    })
+
+    //Initialize the largest possible sum
+    let largestSum = 0                      
+
+    //For each array within the total 2D array
+    total.forEach(arr => {
+        let curSum = 0
+        arr.forEach(value => {      //For each value within the inner array
+            curSum += value         //Add the value given to the curSum for the singular embedded array, this will loop
+        })
+        if (curSum > largestSum) {  //If statement to check if the currentSum is smaller than the largest sum we have, if so, set the largest sum to this number.
+            largestSum = curSum
+        }
+    })
+    console.log(largestSum)
+}
+
+DoPuzzle(input)
